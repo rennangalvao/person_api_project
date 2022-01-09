fdeca8255a54a7bb092b0ab6e598cc06eed9eece
rennangalvao <inc.rennan@gmail.com>
2022-01-09 16:43:15 -0500

    Correção de  erro no PersonController

--- src/main/java/com/rennangalvao/personapi/controller/PersonController.java
+++ src/main/java/com/rennangalvao/personapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.rennangalvao.personapi.dto.MessageResponseDTO;
 import com.rennangalvao.personapi.entity.Person;
 import com.rennangalvao.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,9 +18,10 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person){
 
         return personService.createPerson(person);
     }
-
 }
